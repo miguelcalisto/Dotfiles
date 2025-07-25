c96e248d1967d01fc3e76dc359f8bf63e4252682
miguel <miguelcalistors@gmail.com>
2025-07-25 19:54:40 -0300

    ajustes no alsa

--- scripts/alsa_vol.sh
+++ scripts/alsa_vol.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-volume=$(amixer get Master | grep -oP '\[\d+%\]' | head -1 | tr -d '[]')
+volume=$(amixer get Master | grep -oP '\[\d+%\]' | head -1 | tr -d '[]%')
 status=$(amixer get Master | grep -oP '\[(on|off)\]' | head -1 | tr -d '[]')
 
-if [ "$status" = "off" ]; then
+if [[ "$status" = "off" || "$volume" -eq 0 ]]; then
     echo " 🔇 <span color='#FFB86C'><b>Mutado</b></span> "
 else
-    echo " 🔊 <span color='#FFB86C'><b>${volume}</b></span> "
+    echo " 🔊 <span color='#FFB86C'><b>${volume}%</b></span> "
 fi
 
