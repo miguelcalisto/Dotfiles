ce9d8982209afc983aa03f2ff61d760c2d47c8ce
miguel <miguelcalistors@gmail.com>
2025-08-10 12:30:02 -0300

    add polybar ao install

--- INSTALL.sh
+++ INSTALL.sh
@@ -13,7 +13,7 @@ sudo apt install -y \
     neovim i3 dunst wlogout i3blocks gnome-screenshot rofi fish zsh picom \
     tmux htop btop kitty cava systemd fonts-powerline git curl wget \
     python3-pip ripgrep python3-venv fonts-firacode i3-wm i3lock psensor \
-    alacritty
+    alacritty polybar
 
 # Criar diretórios de configuração
 echo "📁 Criando diretórios de configuração..."
