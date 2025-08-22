509c0eb62abe53a34af647ddfc7f88460257aee3
miguel <miguelcalistors@gmail.com>
2025-08-22 16:35:05 -0300

    att script3.fish

--- SCRIPTS/scripti3.fish
+++ SCRIPTS/scripti3.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/env fish
 
 
-set wallpaper_dir "$HOME/Imagens/topdotop"
+set wallpaper_dir "$HOME/Imagens/Wallpapers"
 #set wallpaper_dir "$HOME/Imagens/bestsWallpapers"
 #set wallpaper_dir "$HOME/Imagens/bestsWallpapers"
 #set wallpaper_dir "$HOME/Imagens/Wallpapers"
@@ -12,7 +12,7 @@ set wallpapers (ls -1S $wallpaper_dir | grep -iE '\.(png|jpe?g|webp)$')  # Agora
 set num (count $wallpapers)
 
 if test $num -eq 0
-    echo "Nenhuma imagem válida encontrada em $wallpaper_dir" >> /home/debian/SCRIPTS/LOGS/logs_scriptDataDoAnoTamanho.log
+    echo "Nenhuma imagem válida encontrada em $wallpaper_dir" >> /home/debian/SCRIPTS/LOGS/log.log
     exit 1
 end
 
@@ -23,10 +23,10 @@ set day_of_year (date +%j)
 set index (math "($day_of_year - 1) % $num + 1")
 set selected "$wallpaper_dir/$wallpapers[$index]"
 
-echo "Mudando para o wallpaper do dia $day_of_year: $selected" >> /home/debian/SCRIPTS/LOGS/logs_scriptDataDoAnoTamanho.log
+echo "Mudando para o wallpaper do dia $day_of_year: $selected" >> /home/debian/SCRIPTS/LOGS/log.log
 
 # Aplica o wallpaper com feh (compatível com i3)
-#feh --bg-scale $selected >> /home/debian/SCRIPTS/LOGS/logs_scriptDataDoAnoTamanho.log 2>&1
+#feh --bg-scale $selected >> /home/debian/SCRIPTS/LOGS/log.log 2>&1
 
-feh --bg-scale "$selected" >> /home/debian/SCRIPTS/LOGS/logs_scriptDataDoAnoTamanho.log 2>&1
+feh --bg-scale "$selected" >> /home/debian/SCRIPTS/LOGS/log.log 2>&1
 
